48dfcaebf72e2f7487156b1cd341b6c07bf585c0
Anna Makarudze <amakarudze@gmail.com>
2022-05-18 00:22:48 +0200

    Remove unused import

--- xmlreader/views.py
+++ xmlreader/views.py
@@ -5,8 +5,6 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.template import loader
 from django.urls import reverse
 
-import pdfkit
-
 from .forms import UploadFileForm
 from .models import Upload
 
